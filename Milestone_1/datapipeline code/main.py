bfd8524baafb3e2049a4e077d18bc21ef54fe610
ThantLwinOo <ngatoe001@gmail.com>
2024-09-16 13:12:47 +0700

    Update main.py

--- Milestone_1/datapipeline code/main.py
+++ Milestone_1/datapipeline code/main.py
@@ -10,3 +10,15 @@ product_lists = data.groupby('InvoiceNo')['Description'].apply(list).tolist()
 # Save product to CSV and create log
 for products in product_lists:
     data_pipe.save_transaction(products)
+
+# Initialize the TrainingPipeline class
+training_pipeline = TrainingPipeline(filepath='retail-data.csv')
+
+# Prepare data for the model
+basket = training_pipeline.prepare_data()
+
+# Train the model with prepared data and get relevant rules
+relevant_rules = training_pipeline.train_model()
+
+# Output the relevant rules
+print(relevant_rules)
